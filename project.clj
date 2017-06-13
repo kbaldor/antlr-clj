236b08f53fee88236d74512b12a4bdd42ad52a86
Kevin Baldor <kbaldor@anduril.electro.swri.edu>
2017-06-13 09:36:31 -0500

    Initial capability

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject antlr-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ANTLR4 bindings for pre-compiled grammars"
+  :url "https://github.com/kbaldor/antlr-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.antlr/antlr4 "4.5.3"]])
